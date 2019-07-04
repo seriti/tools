05e8346974b1c22d2f2aafbca781b7a2aa78a859
Mark <mark@seriti.com>
2019-07-04 12:43:03 +0200

    Config class streamline

--- src/Config.php
+++ src/Config.php
@@ -9,7 +9,7 @@ class Config
       
     public function __construct() 
     {
-        //NB: must come first as constants are referenced in other functions
+        //NB: must come first as constants are referenced in setup()
         $this->constants();
 
         $this->setup();
@@ -27,7 +27,7 @@ class Config
 
         define(__NAMESPACE__.'\CURRENCY_SYMBOL',CURRENCY_SYMBOL);
         
-        //system tables have required cols, see TableStructures class
+        //NB: system tables have required cols, see TableStructures class
         define(__NAMESPACE__.'\TABLE_SYSTEM',TABLE_SYSTEM);
         define(__NAMESPACE__.'\TABLE_AUDIT',TABLE_AUDIT);
         define(__NAMESPACE__.'\TABLE_FILE',TABLE_FILE);
@@ -53,22 +53,11 @@ class Config
         define(__NAMESPACE__.'\URL_CLEAN_LAST',URL_CLEAN_LAST);
     }
 
+    //Not really necessary but shows expected standard configuration layout even if ->set() elsewhere
+    //To check for usage search on "->get('category'" or "->set('category'"
+    //Only specify config for Seriti/Tools/ classes 
     protected function setup()
     { 
-        $this->config['site']['title'] = SITE_NAME;
-        //$this->config['site']['http_root']='urlroot';
-        $this->config['site']['logo'] = 'images/sunflower64.png';
-        $this->config['site']['theme'] = 'DEFAULT';
-
-        //default page settings relative to ['http_root'] if value *.php
-        $this->config['page']['secure'] = 'secure.php';
-        $this->config['page']['home'] = 'index.php';
-
-        //login settings
-        $this->config['login']['page'] = 'index.php';
-        $this->config['login']['template'] = 'login_tmp.php';
-        $this->config['login']['redirect'] = 'user_admin.php';
-
         //database configuration
         $this->config['db']['charset'] = 'utf8';
         $this->config['db']['host'] = '127.0.0.1';
@@ -80,7 +69,6 @@ class Config
         
 
         //email configuration. default method = 'php' too use mail() or 'smtp' to use PhpMailer class
-        
         $this->config['email']['enabled'] = MAIL_ENABLED;
         $this->config['email']['format'] = 'text';
         $this->config['email']['footer'] = '';
@@ -95,51 +83,36 @@ class Config
         $this->config['email']['port'] = MAIL_PORT; //set if default port not valid..ie gmail
         $this->config['email']['secure'] = MAIL_SECURE; //set for 'ssl' or 'tls' if secure smtp connection required
 
-        //user details
-        $this->config['user']['email_from'] = MAIL_FROM;
-        //all possible user access settings, NB sequence is IMPORTANT, GOD > ADMIN > USER > VIEW
-        $this->config['user']['access'] = array('GOD','ADMIN','USER','VIEW');
-        $this->config['user']['status'] = array('OK','HIDE');
-        $this->config['user']['routes'] = array('login'=>'login','logout'=>'login?mode=logout','default'=>'dashboard');
-        $this->config['user']['default_route'] = array('GOD'=>'admin/user','ADMIN'=>'admin/articles','USER'=>'admin/articles','VIEW'=>'admin/articles');
+        //user connfiguration
+        $this->config['user']['access'] = ['GOD','ADMIN','USER','VIEW']; //NB sequence is IMPORTANT, GOD > ADMIN > USER > VIEW
+        $this->config['user']['status'] = ['OK','HIDE'];
+        //$this->config['user']['routes'] = ['login'=>'login','logout'=>'login?mode=logout','default'=>'dashboard'];
+        //$this->config['user']['default_route'] = ['GOD'=>'admin/user','ADMIN'=>'admin/dashboard','USER'=>'admin/dashboard','VIEW'=>'admin/dashboard'];
 
         
-        //Audit setting
+        //Audit setting *** used ***
         $this->config['audit']['enabled'] = AUDIT;
         $this->config['audit']['table'] = TABLE_AUDIT;
         $this->config['audit']['table_exclude'] = [];
 
+        
         //default cache settings are user specific
         $this->config['cache']['type'] = 'MYSQL';
-        $this->config['cache']['dir'] = 'admin/cache/'; //directory must be writeable
-        $this->config['cache']['ext'] = '.cache';
-        $this->config['cache']['table'] = 'cache';
+        $this->config['cache']['table'] = TABLE_CACHE;
+        //$this->config['cache']['type'] = 'FILE';
+        //$this->config['cache']['dir'] = 'admin/cache/'; //directory must be writeable
+        //$this->config['cache']['ext'] = '.cache';
+        
 
-        //Amazon S3 settings
+        //Amazon S3 settings *** used ***
         $this->config['s3']['region'] = AWS_S3_REGION;
         $this->config['s3']['key'] = AWS_S3_KEY;
         $this->config['s3']['secret'] = AWS_S3_SECRET;
         $this->config['s3']['bucket'] = AWS_S3_BUCKET;
         $this->config['s3']['debug'] = false;
-        
-
-        //template settings  
-        $this->config['templates']['dir'] = 'templates/';
-
-        //error handling settings
-        $this->config['errors']['handle'] = false;
+       
+        //sms sending
         /*
-        $this->config['errors']['redirect']=true;
-        $this->config['errors']['page']=''; //BASE_URL.'admin/error';
-        $this->config['errors']['email']=MAIL_ENABLED;
-        $this->config['errors']['email_subject']=SITE_TITLE.'***ERROR***';
-        $this->config['errors']['email_from']=MAIL_FROM;
-        $this->config['errors']['email_to']=MAIL_WEBMASTER;
-        $this->config['errors']['template']='error_tmp.php';
-        $this->config['errors']['audit']=true;
-        */
-
-        //sms capabilities
         $this->config['sms']['isp'] = 'CLICKATELL'; //supports CLICKATELL or PC2SMS
         $this->config['sms']['protocol'] = 'HTTP';
         $this->config['sms']['user'] = CLICKATELL_USER;
@@ -147,9 +120,10 @@ class Config
         $this->config['sms']['http_base'] = 'https://api.clickatell.com';
         $this->config['sms']['http_method'] = 'CURL';
         $this->config['sms']['http_post'] = false;
-        //$this->config['sms']['footer']=DISCLAIMER_URL;
+        $this->config['sms']['footer']=DISCLAIMER_URL;
         $this->config['sms']['api_id'] = CLICKATELL_API_ID;//CLICKATELL specific
         $this->config['sms']['account'] = '';//PC2SMS specific
+        */
     }
 
 
@@ -165,8 +139,18 @@ class Config
         return false;
     }
 
-    public function set($section,$key,$value) 
+    //only allows overwrite of existing values if $replace = true
+    public function set($section,$key,$value,$replace = false) 
     {
+        if(!$replace) {
+           if(isset($this->config[$section][$key])) {
+                $msg = 'You cannot replace an existing seriti config value';
+                if(DEBUG) $msg .= 'for section['.$section.'] key['.$key.']';
+                die($msg);
+                exit;
+           }  
+        }
+
         $this->config[$section][$key] = $value;
     }
 
